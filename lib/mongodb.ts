a053c192a47d4878178179be08eba705a8161eb6
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-12-24 18:33:49 +0700

    .

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -9,6 +9,9 @@ const options = {}
 
 let client
 let clientPromise: Promise<MongoClient>
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
